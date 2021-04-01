da9f06c89f4b84c7a4e2e92520ca671913721730
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-01 20:11:01 +0200

    docs: :construction: Construction of the project
    I created the structure of the project, which will help us to make the logic of the program later

---
+++ RomansGoHome/src/main/java/edu/pingpong/RomansGoHome/NumeroRomano.java
@@ -0,0 +1,13 @@
+package edu.pingpong.RomansGoHome;
+
+/**
+ * Hello world!
+ *
+ */
+public class NumeroRomano 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
